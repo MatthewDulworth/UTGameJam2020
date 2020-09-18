26dcd74f3351c9c9686644404c2f4b1fb6e115e6
Matthew Dulworth <mhdulworth@gmail.com>
2020-09-17 20:13:04 -0500

    Added player movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Player : MonoBehaviour {
+    
+    public float speed = 10;
+    private Rigidbody2D rb;
+    
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        HandleMovement();
+    }
+
+    // handles movement
+    private void HandleMovement() {
+        
+        // horizontal
+        int x = 0;
+        if (Input.GetKey(KeyCode.A)) {
+            x += -1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            x += 1;
+        }
+        
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+    }
+}
