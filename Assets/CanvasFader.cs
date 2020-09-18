a482a95acd84efed68bee761c84ce491e1a029cc
Matthew Dulworth <mhdulworth@gmail.com>
2020-09-17 23:23:57 -0500

    merge

---
+++ Assets/CanvasFader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Collider2D))]
+public class CanvasFader : MonoBehaviour
+{
+
+    public Image target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.ToLower().Equals("player"))
+        {
+            Debug.Log("Entry");
+            target.CrossFadeAlpha(1f, 2f, true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag.ToLower().Equals("player"))
+        {
+            Debug.Log("Exit");
+            target.CrossFadeAlpha(0f, 2f, true);
+        }
+
+    }
+}
