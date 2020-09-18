49abe73350c869b64e8440f485c3dfc721d44121
Matthew Dulworth <mhdulworth@gmail.com>
2020-09-18 11:00:24 -0500

    Grapple physics and line renderer

--- Assets/Editor/PlayerEditor.cs
+++ Assets/Editor/PlayerEditor.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEngine.Rendering.VirtualTexturing;
 
-[CustomEditor (typeof(Player))]
-public class PlayerEditor : Editor
-{
-   void OnSceneGUI(){
-      Player player = (Player)target;
+[CustomEditor(typeof(Player))]
+public class PlayerEditor : Editor {
+    void OnSceneGUI() {
+        Player player = (Player) target;
 
-      Handles.color = Color.green;
-      Handles.DrawWireDisc(player.transform.position, Vector3.back, player.grappleRadius);
-   }
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(player.transform.position, Vector3.back, player.grappleRadius);
+
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(player.transform.position + (Vector3) player.bottomOffset,
+            Vector3.back, player.collisionRadius);
+    }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,68 +1,46 @@
-﻿using System;
-using System.Data.SqlTypes;
+﻿using System.Net.Configuration;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour {
-    public float speed = 10;
-    
-    public float clickRadius = 1;
-    public float grappleRadius = 5;
-    public float grappleSpeed;
-    
-    public LayerMask targetLayer;
-    public LayerMask obstacleLayer;
-    
-    private Rigidbody2D rb;
+    // --- public vars --- //
+    public float groundSpeed;
+    public float airAcceleration;
+    public float grappleAccel;
+    public float maxSpeed;
+    public float grappleRadius;
+    public float collisionRadius;
+    public Vector2 bottomOffset;
 
-    private bool grappling = false;
+    public LayerMask targetLayer;
+    public LayerMask groundLayer;
+
+    // --- private vars --- //
+    private bool grappling;
     private Vector2 grapplePoint;
+    private Rigidbody2D rb;
+    private LineRenderer lineRenderer;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     private void FixedUpdate() {
-        HandleMovement();
         Grapple();
-        //HandleGrapple();
-    }
 
-    private void Grapple() {
-        
-        if (Input.GetKeyDown(KeyCode.J)) {
-            Vector2 origin = transform.position;
-            Vector2 target = ClosestGrappleInRange(origin);
-
-            if (target != origin) {
-                rb.AddForce((target - origin) * grappleSpeed, ForceMode2D.Impulse);
-            }
+        if (Grounded()) {
+            GroundMovement();
+        }
+        else {
+            // AirMovement();
         }
     }
 
-    private Vector2 ClosestGrappleInRange(Vector2 origin) {
-        Collider2D[] grapplePoints = Physics2D.OverlapCircleAll(transform.position, grappleRadius, targetLayer);
-        
-        if (grapplePoints.Length > 0) {
-            float closestDist = Mathf.Infinity;
-            Vector2 closestPoint = grapplePoints[0].transform.position;
 
-            foreach (Collider2D collide in grapplePoints) {
-                Vector2 point = collide.transform.position;
-                Vector2 direction = (point - origin).normalized;
-                float dist = Vector2.Distance(point, origin);
-
-                if (dist < closestDist && !Physics2D.Raycast(origin, direction, dist, obstacleLayer)) {
-                    closestDist = dist;
-                    closestPoint = collide.transform.position;
-                }
-            }
-            return closestPoint;
-        }
-        return origin;
-    }
-    
-    private void HandleMovement() {
+    // --------------------------------------------------------------
+    // Air Movement
+    // --------------------------------------------------------------
+    private void AirMovement() {
         int x = 0;
         if (Input.GetKey(KeyCode.A)) {
             x += -1;
@@ -72,45 +50,80 @@ public class Player : MonoBehaviour {
             x += 1;
         }
 
-        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+        rb.AddForce(new Vector2(x, 0) * airAcceleration);
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
     }
 
-    // private Vector2 GrappelPoint() {
-    //     Vector3 pos = Input.mousePosition;
-    //     pos.z = Camera.main.nearClipPlane;
-    //     Vector2 mousePos = Camera.main.ScreenToWorldPoint(pos);
-    //
-    //     // mouse pos within grapple radius
-    //     if (Vector2.Distance(mousePos, transform.position) <= grappleRadius) {
-    //          Collider2D[] grapplePoints = Physics2D.OverlapCircleAll(mousePos, clickRadius, targetLayer);
-    //          if (grapplePoints.Length > 0) {
-    //             Targeting.GetClosestTarget(grapplePoints, mousePos) 
-    //          }
-    //     }
-    // }
-    
-     // private void HandleGrapple() {
-    //     if (!grappling && Input.GetKeyDown(KeyCode.J)) {
-    //         Transform point = fov.ClosestTarget();
-    //         if (point) {
-    //             grappling = true;
-    //             grapplePoint = point.position;
-    //             Debug.Log("start");
-    //         }
-    //     }
-    //     else if (Input.GetKeyUp(KeyCode.J)) {
-    //         grappling = false;
-    //         Debug.Log("end");
-    //     }
-    //
-    //     if (grappling) {
-    //         Vector2 dir = (grapplePoint - (Vector2) transform.position).normalized;
-    //         rb.AddForce(speed * dir, ForceMode2D.Force);
-    //         Debug.Log("on-going");
-    //         if (Vector2.Distance(transform.position, grapplePoint) < 0.1) {
-    //             grappling = false;
-    //             Debug.Log("end");
-    //         }
-    //     }
-    // }
+    // --------------------------------------------------------------
+    // Grappling
+    // --------------------------------------------------------------
+    private void Grapple() {
+        Vector2 origin = transform.position;
+
+        if (!grappling && Input.GetKeyDown(KeyCode.J)) {
+            grapplePoint = ClosestGrappleInRange(origin);
+
+            if (grapplePoint != origin) {
+                grappling = true;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.J) && grappling) {
+            Vector2 dir = (grapplePoint - origin).normalized;
+            rb.AddForce(dir * grappleAccel);
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, origin);
+            lineRenderer.SetPosition(1, grapplePoint);
+        }
+        else {
+            grappling = false;
+            lineRenderer.positionCount = 0;
+        }
+    }
+
+    private Vector2 ClosestGrappleInRange(Vector2 origin) {
+        Collider2D[] grapplePoints = Physics2D.OverlapCircleAll(transform.position, grappleRadius, targetLayer);
+
+        if (grapplePoints.Length > 0) {
+            float closestDist = Mathf.Infinity;
+            Vector2 closestPoint = transform.position;
+
+            foreach (Collider2D collide in grapplePoints) {
+                Vector2 point = collide.transform.position;
+                Vector2 direction = (point - origin).normalized;
+                float dist = Vector2.Distance(point, origin);
+
+                if (dist < closestDist && !Physics2D.Raycast(origin, direction, dist, groundLayer)) {
+                    closestDist = dist;
+                    closestPoint = collide.transform.position;
+                }
+            }
+
+            return closestPoint;
+        }
+
+        return transform.position;
+    }
+
+    // --------------------------------------------------------------
+    // Grounded Movement
+    // --------------------------------------------------------------
+    private void GroundMovement() {
+        int x = 0;
+        if (Input.GetKey(KeyCode.A)) {
+            x += -1;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            x += 1;
+        }
+
+        rb.velocity = new Vector2(x * groundSpeed, rb.velocity.y);
+    }
+
+    private bool Grounded() {
+        return Physics2D.OverlapCircle((Vector2) transform.position + bottomOffset, collisionRadius, groundLayer);
+    }
 }
