c24fecba57e684623c56a8f4f1b0fe96c5e25679
Neepsy <eric55664@gmail.com>
2020-09-17 21:53:39 -0500

    Updrafts
    Added scripts for updrafts and "perfect" boosts.

---
+++ Assets/Booster.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Booster : MonoBehaviour
+{
+
+    public float boostForce = 1000f;
+    private Boolean canBoost = false;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp("k") && canBoost)
+        {
+            rb.AddForce(Vector2.up * boostForce);
+            Debug.Log("Woooosh!");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("UpdraftTop"))
+        {
+            canBoost = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("UpdraftTop"))
+        {
+            canBoost = false;
+        }
+    }
+}
---
+++ Assets/Updraftable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Updraftable : MonoBehaviour
+{
+
+    public Boolean willUpdraft = true;
+    [SerializeField]
+    private Boolean isPlayer = false;
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("k") && isPlayer)
+        {
+            willUpdraft = true;
+        }
+        else if(isPlayer)
+        {
+            willUpdraft = false;
+        }
+    }
+
+    public Rigidbody2D rigidBody()
+    {
+        return rb;
+    }
+}
