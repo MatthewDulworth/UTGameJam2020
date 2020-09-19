06973d9a2df79aeba15e4031bca91edba45c806a
Neepsy <eric55664@gmail.com>
2020-09-18 23:47:38 -0500

    Respawn system

---
+++ Assets/Scripts/Player/Respawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+    public float respawnHeight = -40f;
+    private Vector3 respawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPoint = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y <= respawnHeight)
+        {
+            respawn();
+        }
+    }
+
+    public void respawn()
+    {
+        gameObject.transform.position = respawnPoint;
+    }
+
+    public void setRespawnPoint(Vector3 point)
+    {
+        respawnPoint = point;
+    }
+}
