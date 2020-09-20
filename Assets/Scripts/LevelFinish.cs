bd87388bdf49717b8b0906d68b10adf30d87110e
Neepsy <eric55664@gmail.com>
2020-09-19 23:09:09 -0500

    Script to switch to next level

---
+++ Assets/Scripts/LevelFinish.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D)]
+public class LevelFinish : MonoBehaviour
+{
+    [Tooltip("Check the build settings for the indexs")]
+    public int targetSceneIndex;
+
+    public float fadeInTime = 2f;
+    public float fadeOutTime = 1f;
+
+    public float delay = 5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.ToLower().Equals("player"))
+        {
+            StartCoroutine(switchDelay());
+        }
+    }
+
+    IEnumerator switchDelay()
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        SceneSwitcher.Instance().loadScene(targetSceneIndex, fadeOutTime, fadeInTime);
+    }
+}
